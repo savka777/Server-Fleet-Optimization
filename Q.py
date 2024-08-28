39510a357a48b30f91e4bf41993a2d7cc65773d3
Fernando <a00289109>
2024-08-28 10:59:32 +0100

    Change Approaching to the move_server function

--- Q.py
+++ Q.py
@@ -292,20 +292,29 @@ def move_server(state, server_type, from_datacenter_id, to_datacenter_id):
     from_datacenter = state['datacenters'][from_datacenter_id]
     # Get the datacenter we want to move the servers to
     to_datacenter = state['datacenters'][to_datacenter_id]
-    
+
     # Make sure we have those servers
-    if from_datacenter['allocated_servers'][server_type] > 0:
+    if from_datacenter['allocated_servers'][server_type] <= 0:
+
+        choose_action(state, actions)
+
+        action_results = {
+            'success': False,
+            'penalty': -50  # Arbitrary penalty value
+        }
+
+    else:
         # Make sure the destination datacenter has enough capacity
         if to_datacenter['slots_capacity'] >= server_slots:
             # Update, remove the servers from datacenter
             from_datacenter['allocated_servers'][server_type] -= 1
             # Update, add the amounts of slots avaliable
             from_datacenter['slots_capacity'] += server_slots
-            
+
             # Update the new datacenter
             to_datacenter['allocated_servers'][server_type] += 1
             to_datacenter['slots_capacity'] -= server_slots
-            
+
             # Transfer the operating time for the server
             to_datacenter['operating_times'][server_type] = from_datacenter['operating_times'][server_type]
             server_to_move = None
@@ -313,7 +322,7 @@ def move_server(state, server_type, from_datacenter_id, to_datacenter_id):
                 if server['server_generation'] == server_type:
                     server_to_move = server
                     break
-            
+
             if server_to_move:
                 from_datacenter['fleet'].remove(server_to_move)
                 server_to_move['datacenter_id'] = to_datacenter_id
@@ -327,24 +336,21 @@ def move_server(state, server_type, from_datacenter_id, to_datacenter_id):
             print(f"Moved {server_type} from {from_datacenter_id} to {to_datacenter_id}. Updated Obj: {O}")
 
             action_results = {
-                'success': True,  
-                'penalty': 0     
-                }
+                'success': True,
+                'penalty': 0
+            }
         else:
-            print(f"Cannot move server {server_type} from datacenter {from_datacenter_id} to {to_datacenter_id}: Not enough capacity.")
-            excess_slots = server_slots - to_datacenter['slots_capacity']
-            penalty = excess_slots * 10 
+
+            choose_action(state, actions)
+
+            penalty = excess_slots * 10
             action_results = {
-                'success': False,  
+                'success': False,
                 'penalty': penalty
             }
-    else:
-        print(f"Cannot move server {server_type}: No such server in datacenter {from_datacenter_id}.")
-        action_results = {
-            'success': False,  
-            'penalty': -50     # Arbitrary penalty value
-        }
+
     return state, action_results
+
 
 # When server retires replace if needed (not sure if we need this yet)
 def retire_and_replace_server(state, server_type, datacenter_id):
